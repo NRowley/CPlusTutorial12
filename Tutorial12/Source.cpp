8956a0c6fa887ca558cbdadf91d0fbe907ff911b
NRowley <nicholas.ryan.rowley@gmail.com>
2023-11-16 12:53:44 -0600

    Managing and deleting memory on the Heap

--- Tutorial12/Source.cpp
+++ Tutorial12/Source.cpp
@@ -2,13 +2,32 @@
 #include <string>
 using namespace std;
 
+struct Character
+{
+	Character() {
+		Name = "Default";
+		Health = 100.f;
+	}
 
+	string Name;
+	float Health;
+
+	float getHealth() {
+		return Health;
+	}
+};
 
 
 int main() {
-
-
-
+	for (int i = 0; i < 10000000; i++) {
+		Character* PtrToChar = new Character();
+		PtrToChar->Name = "Killian " + i;
+		cout << PtrToChar->Name << endl;
+		cout << PtrToChar->getHealth() << endl;
+		//delete the ptr to free up memory
+		delete PtrToChar;
+	}
+	
 
 	system("pause");
 }
